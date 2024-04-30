9e32f2633776744f1db52903df5e9dcfc0434bb7
Paweł bbkr Pabian <bbkr@post.pl>
2024-04-30 02:47:19 +0200

    Constructor with tests.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,39 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::collections::HashMap;
+
+pub struct SquishyID {
+    length: usize,
+    characters_to_positions: HashMap<char, usize>,
+    positions_to_characters: Vec<char>
+}
+
+impl SquishyID {
+
+    fn new (key: &str) -> Result<Self, &str> {
+
+        let positions_to_characters: Vec<char> = key.chars().collect();
+
+        let length: usize = positions_to_characters.len();
+        if length < 2 {
+            return Err( "Key must contain at least 2 characters." );
+        }
+
+        let mut characters_to_positions: HashMap<char, usize> = HashMap::new();
+        for (position, &character) in positions_to_characters.iter().enumerate() {
+            if characters_to_positions.insert( character, position ).is_some() {
+                return Err( "Key must contain unique characters." );
+            }
+
+        }
+
+        Ok(
+            Self {
+                length,
+                characters_to_positions,
+                positions_to_characters
+            }
+        )
+    }
+
 }
 
 #[cfg(test)]
@@ -7,8 +41,22 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn key_too_short() {
+        assert!(matches!(SquishyID::new(""), Err("Key must contain at least 2 characters.")));
+        assert!(matches!(SquishyID::new("a"), Err("Key must contain at least 2 characters.")));
     }
+
+
+    #[test]
+    fn key_not_unique() {
+        assert!(matches!(SquishyID::new("aa"), Err("Key must contain unique characters.")));
+        assert!(matches!(SquishyID::new("aba"), Err("Key must contain unique characters.")));
+    }
+
+    #[test]
+    fn valid_key() {
+        assert!(SquishyID::new( "ab" ).is_ok());
+    }
+
+
 }
